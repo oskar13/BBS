007ef1d415fbcca1dda274c31b43aa5d6bc38db4
oskar13 <oskar92@tlu.ee>
2013-12-27 03:43:46 +0200

    update

---
+++ admin_post.php
@@ -0,0 +1,21 @@
+<?php
+require('config.php');
+session_set_cookie_params(1200, '/bbs');
+session_start();
+
+if(!isset($_SESSION['user_ID'])) {
+    header("Location: login.php");
+    exit();
+}
+
+////////////////////////////////////////////////////////////
+//////////        SITE SETTINGS UPDATE       ///////////////
+////////////////////////////////////////////////////////////
+if (isset($_POST['site_title'])) {
+    try {
+        $stmt = $conn->prepare("SELECT post_ID FROM POSTS");
+        $stmt->execute();
+        } catch(PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
+}
